77b3f75aec0f4e7fb07dfca5e01be3e0463b4e6a
Nidhi kumari <190101120115@cutm.ac.in>
2022-11-09 13:12:21 +0530

    added sports and responsibility forms, login ui changes

--- backend/csr/src/main/java/com/springboot/csr/repository/MusicClubRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.csr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.csr.model.MusicClub;
-
-public interface MusicClubRepository extends JpaRepository<MusicClub, Long> {
-
-}
